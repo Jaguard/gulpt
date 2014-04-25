65e0c2a5082c42dd132ebe543c0098bdec14dfa2
Ciprian Popa <ciprian.popa@jaguard.com>
2014-04-25 13:14:23 +0300

    Try to fix #1 (https://github.com/jaguard/gulpt/issues/1)

--- bin/gulpt.js
+++ bin/gulpt.js
@@ -3,7 +3,7 @@
 var which = require("which"),
 		spawn = require("child_process").spawn,
 		args = process.argv;
-		
+
 // keep just params
 args.splice(0, 2);
 // add time-require module
@@ -12,8 +12,8 @@ args.push(require.resolve("time-require"));
 
 // call gulp using 'which' to avoid this issue on Windows: https://github.com/joyent/node/issues/3968
 which("gulp", function(err, gulpPath){
-  if (err) {
+	if (err) {
 		throw new Error(err);
-  }
+	}
 	spawn(gulpPath, args, { stdio: "inherit" });
 });
